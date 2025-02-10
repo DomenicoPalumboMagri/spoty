4aa9d6f7ff9108e20ea766e45c221be8189e2b30
DomenicoPalumboMagri <palumbo.magri.d@iisgalvanimi.edu.it>
2025-02-10 12:00:02 +0000

    Messaggio del commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "033d14b0d2cd48068699ee4bad749e9b"
 SPOTIFY_CLIENT_SECRET = "e4ce5171826d4cbe8a7d30c84dec6c5d"
-SPOTIFY_REDIRECT_URI = "https://5000-domenicopalumbom-spoty-pmjmztv9hv5.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-domenicopalumbom-spoty-k9i36n82p1b.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
@@ -15,7 +15,8 @@ sp_oauth = SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
     redirect_uri=SPOTIFY_REDIRECT_URI,
-    scope="user-read-private" #permessi x informazioni dell'utente
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 
 @app.route('/')
@@ -30,6 +31,7 @@ def callback():
     session['token_info'] = token_info #salvo il token nella mia sessione x riutilizzarlo
     return redirect(url_for('home'))
 
+
 @app.route('/home')
 def home():
     token_info = session.get('token_info', None) #recupero token sissione (salvato prima)
@@ -38,7 +40,16 @@ def home():
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
     print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
-
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
+    
+    return render_template('home.html', user_info=user_info, playlists=playlists_info)
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
+@app.route('/playlist_item')
+def playlist_item():
+    
 if __name__ == '__main__':
     app.run(debug=True)
